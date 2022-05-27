5864e11eeead46d88b5a5d8f3b1b343fd2b01b3f
Leandro Costa <lcostasantos1987@gmail.com>
2022-05-26 22:06:09 -0300

    Os Itens Duplicados no array
    Os Itens Duplicados no array

--- arraysExercicios/src/main/java/exercios08/Exercicios08.java
+++ arraysExercicios/src/main/java/exercios08/Exercicios08.java
@@ -10,5 +10,19 @@ package exercios08;
  */
 public
         class Exercicios08 {
-    
+              public static void main(String[] args) 
+    {
+        int[] my_array = {1, 2, 5, 5, 6, 6, 7, 2};
+ 
+        for (int i = 0; i < my_array.length-1; i++)
+        {
+            for (int j = i+1; j < my_array.length; j++)
+            {
+                if ((my_array[i] == my_array[j]) && (i != j))
+                {
+                    System.out.println(" Os Itens Duplicados São : "+my_array[j]);
+                }
+            }
+        }
+    }    
 }
