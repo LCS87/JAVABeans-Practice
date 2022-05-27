2da92bb1332f8e6c42f03320c266830be31a339a
Leandro Costa <lcostasantos1987@gmail.com>
2022-05-26 22:16:13 -0300

    array em arrarylist
    array em arraylist

--- arraysExercicios/src/main/java/exercios09/Exercicios09.java
+++ arraysExercicios/src/main/java/exercios09/Exercicios09.java
@@ -4,11 +4,21 @@
  */
 package exercios09;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
- *
+ *Convertendo Array em ArrayList
  * @author leandro
  */
 public
         class Exercicios09 {
-    
+              
+              public static void  main(String[] args) {
+                  
+              String[]  my_array = new String[] {"Python", "JAVA", "PHP",  "Perl", "C#", "C++"};
+              ArrayList<String>  list = new ArrayList<String>(Arrays.asList(my_array));
+  
+              System.out.println(list);
+  }
 }
