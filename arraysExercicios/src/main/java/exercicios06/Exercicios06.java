dd1a64c401289ec926f1ea75a7f2cede7b08836c
Leandro Costa <lcostasantos1987@gmail.com>
2022-05-26 23:19:36 -0300

    correçaõ de nome
    correçaõ de nome

--- arraysExercicios/src/main/java/exercicios06/Exercicios06.java
+++ arraysExercicios/src/main/java/exercicios06/Exercicios06.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package exercios06;
+package exercicios06;
 import java.util.Arrays; 
 /**
  * Localizando o maior e menor numero no array
